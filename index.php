a5e8d0a736b8db6644a32689b8246c913a74cda0
Ashraful Kalam Abid <abid7ctg@gmail.com>
2021-03-08 23:37:11 +0600

    Index file added

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<div class="row justify-content-center">
+		<form action="" method="POST">
+	<div class="form-group">
+		<label>Name</label>
+		<input type="text" name="name" class="form-control" value="Enter Your name">	
+	</div>
+	<div class="form-group">
+		<label>Location</label>
+		<input type="text" name="location" class="form-control" value="Enter Your location">
+	</div>
+	<div class="form-group">
+		<button class="btn btn-primary" type="submit" name="save">Save</button>
+	</div>
+	 	</form>
+	</div>
+</body>
+</html>
