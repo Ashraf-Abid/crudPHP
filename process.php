c81c7a20191006ed0986d13555cca02e10885a96
Ashraful Kalam Abid <abid7ctg@gmail.com>
2021-03-15 01:13:54 +0600

    Delete feature added

--- process.php
+++ process.php
@@ -6,7 +6,14 @@ $name=$_POST['name'];
 $location=$_POST['location'];
 $mysqli->query("insert into data(name,location) VALUES ('$name','$location')")or die($mysqli->error);
 header('location:index.php');
+}
+if(isset($_GET['delete'])){
+
+	$id=$_GET['delete'];
+	$mysqli->query("DELETE FROM data WHERE Id=$id") or die($mysqli->error());
+header('location:index.php');
 
 }
 
-?>
+
+
